3ee164eedb8ebbc622b013f88f11aba56ab6df79
Lana00sophia <sophialanasilvadaluz@gmail.com>
2025-09-04 19:41:02 +0000

    lalala

---
+++ funcoesexe.py
@@ -0,0 +1,25 @@
+def saudacao():
+    print("Olá,bem vindo ao Python!")
+saudacao()
+
+def dobro(a):
+    return a * 2
+print(dobro(2))
+print(dobro(3))
+print(dobro(5))
+def soma(a,b):
+    return a + b 
+print(soma(10,20))
+def mensagem(nome = "vitante"):
+    print("Olá "+ nome + "!")
+mensagem("Sophia")
+mensagem()
+
+def operacoes(a,b):
+    opera = int(input("Operação"))
+
+
+
+
+
+
